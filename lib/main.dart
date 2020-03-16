80f470831598a7364a7a1184d13f89382d8c0784
Matthew Coetzee <18coetma@wbhs.co.za>
2020-03-16 10:49:02 +0200

    Test Class Extension

--- lib/main.dart
+++ lib/main.dart
@@ -123,6 +123,20 @@ class _CameraHomeScreenState extends State<CameraHomeScreen> {
   }
 }
 
+class TestClass extends StatefulWidget {
+  @override
+  _TestClassState createState() => _TestClassState();
+}
+
+class _TestClassState extends State<TestClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
 
 
 class SelectionOption extends StatefulWidget {
