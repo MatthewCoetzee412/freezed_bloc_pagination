c99e485277c70d0c410fd4e46491a19bffd19050
Matthew Coetzee <18coetma@wbhs.co.za>
2020-03-16 12:01:51 +0200

    Login Intial Feature

---
+++ lib/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LOGIN extends StatefulWidget {
+  @override
+  _LOGINState createState() => _LOGINState();
+}
+
+class _LOGINState extends State<LOGIN> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
